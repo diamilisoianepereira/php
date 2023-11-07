aabf7dfab16273e80bc1a46eafceeca91e18b283
Milly <145469893+diamilisoianepereira@users.noreply.github.com>
2023-11-07 11:58:59 -0300

    Add files via upload

---
+++ aula02/exp01.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp01.css">
+    <style>
+    h1 {
+background-color: blueviolet;
+}
+p{
+    background-color: brown;
+}
+
+h2{
+    background-color: aquamarine;
+}
+
+p{
+    background-color: aqua;
+}
+</style>
+</head>
+   <h1> jaden </h1>
+    <p> jaden </p>
+    <h2> jaden </h2>
+    <p> jaden </p>
+
+</body>
+</html>
+
---
+++ aula02/exp02.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp01.css">
+    <title>Document</title>
+</head>
+<body>
+   <h1> jaden </h1>
+    <p> 
+        texto 1 .... </p>
+
+    <h1 id="h1_2"> jaden 2 </h2>
+    <p id="p2">
+        texto 2 .... </p>
+
+        <h1 class="estilo"> jaden 3 </h2>
+        <p class = "estilo">
+            texto 3 ....</P>
+
+        <h1 class="estilo"> jaden 4 </h2>
+        <p clas = "estilo">
+            texto 4 ....
+</p>
+
+</body>
+</html>
