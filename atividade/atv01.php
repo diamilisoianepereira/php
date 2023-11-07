bb0d3571dd7fdf0297d6e0d2360f079d8ddcb8c2
Milly <145469893+diamilisoianepereira@users.noreply.github.com>
2023-11-07 11:59:39 -0300

    Add files via upload

---
+++ atividade/atv01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1 {}
+
+    </style>
+</head>
+<body>
+    <h1 style="background-color: blueviolet;"> jaden </h1>
+    <p style="background-color: brown;"> jaden </p>
+    <h2 style="background-color: aquamarine;"> jaden </h2>
+    <p style="background-color: aqua;"> jaden </p>
+</body>
+</html>
