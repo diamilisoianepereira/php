b24b67f0f420e03dec836320d7462c14562b5099
Milly <145469893+diamilisoianepereira@users.noreply.github.com>
2024-05-03 13:35:02 -0300

    Add files via upload

---
+++ aula-30-04/aluno.php
@@ -0,0 +1,42 @@
+<?php
+$aluna["nome"] = "diamili";
+echo "aluna : $aluna[nome] <br>";
+$aluna["idade"] = 17;
+echo "idade : $aluna[idade] <br>";
+$aluna["endereco"] = "xxxxxxx";
+echo "endereco : $aluna[endereco] <br>";
+$aluna["cidade"] = "palmas de monte alto";
+echo "cidade : $aluna[cidade] <br>";
+$aluna["estado"] = "Bahia";
+echo "estado : $aluna[estado] <br>";
+$aluna["CPF"] = 768;
+echo "CPF : $aluna[CPF] <br>";
+
+echo"<p>aluna</p> <br>";
+$aluna["nome"] = "etephane";
+echo "aluna : $aluna[nome] <br>";
+$aluna["idade"] = 17;
+echo "idade : $aluna[idade] <br>";
+$aluna["endereco"] = "xxxxxxx";
+echo "endereco : $aluna[endereco] <br>";
+$aluna["cidade"] = "palmas de monte alto";
+echo "cidade : $aluna[cidade] <br>";
+$aluna["estado"] = "Bahia";
+echo "estado : $aluna[estado] <br>";
+$aluna["CPF"] = 632;
+echo "CPF : $aluna[CPF] <br>";
+
+echo"<p>aluno</p> <br>";
+$aluno["nome"] = "guilherme";
+echo "aluno : $aluno[nome] <br>";
+$aluno["idade"] = 17;
+echo "idade : $aluno[idade] <br>";
+$aluno["endereco"] = "xxxxxxx";
+echo "endereco : $aluno[endereco] <br>";
+$aluno["cidade"] = "cacaule";
+echo "cidade : $aluno[cidade] <br>";
+$aluno["estado"] = "Bahia";
+echo "estado : $aluno[estado] <br>";
+$aluno["CPF"] = 198;
+echo "CPF : $aluno[CPF] <br>";
+?>
---
+++ aula-30-04/professores.php
@@ -0,0 +1,42 @@
+<?php
+$professor["nome"] = "Fabio";
+echo "professor : $professor[nome] <br>";
+$professor["idade"] = 44;
+echo "idade : $professor[idade] <br>";
+$professor["endereco"] = "xxxxxxx";
+echo "endereco : $professor[endereco] <br>";
+$professor["cidade"] = "Guanambi";
+echo "cidade : $professor[cidade] <br>";
+$professor["estado"] = "Bahia";
+echo "estado : $professor[estado] <br>";
+$professor["CPF"] = 897;
+echo "CPF : $professor[CPF] <br>";
+
+echo"<p>professor</p> <br>";
+$professor["nome"] = "ivanilson";
+echo "professor : $professor[nome] <br>";
+$professor["idade"] = 40;
+echo "idade : $professor[idade] <br>";
+$professor["endereco"] = "xxxxxxx";
+echo "endereco : $professor[endereco] <br>";
+$professor["cidade"] = "Guanambi";
+echo "cidade : $professor[cidade] <br>";
+$professor["estado"] = "Bahia";
+echo "estado : $professor[estado] <br>";
+$professor["CPF"] = 980;
+echo "CPF : $professor[CPF] <br>";
+
+echo"<p>professor</p> <br>";
+$professor["nome"] = "eber";
+echo "professor : $professor[nome] <br>";
+$professor["idade"] = 38;
+echo "idade : $professor[idade] <br>";
+$professor["endereco"] = "xxxxxxx";
+echo "endereco : $professor[endereco] <br>";
+$professor["cidade"] = "vitoria da conquista";
+echo "cidade : $professor[cidade] <br>";
+$professor["estado"] = "Bahia";
+echo "estado : $professor[estado] <br>";
+$professor["CPF"] = 076;
+echo "CPF : $professor[CPF] <br>";
+?>
